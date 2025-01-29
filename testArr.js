4c13f1300fc3f22588f5a0304922c91ed078592d
gowthamRaj-sudo <gowthamraj87544@gmail.com>
2025-01-29 19:15:55 +0530

    sdfgh

--- testArr.js
+++ testArr.js
@@ -2,10 +2,11 @@ module.exports={
 
     data:[
 
-        {id:1,image:'https://www.google.com/imgres?q=images&imgurl=https%3A%2F%2Fletsenhance.io%2Fstatic%2F73136da51c245e80edc6ccfe44888a99%2F1015f%2FMainBefore.jpg&imgrefurl=https%3A%2F%2Fletsenhance.io%2F&docid=-t22bY2ix3gHaM&tbnid=D2e1clQQJsbJwM&vet=12ahUKEwjRx__I6JqLAxVyxzgGHYKKCPYQM3oECHIQAA..i&w=1280&h=720&hcb=2&ved=2ahUKEwjRx__I6JqLAxVyxzgGHYKKCPYQM3oECHIQAA',
+        {id:1,
 
-        },
-        {id:2,image:'https://www.google.com/imgres?q=images&imgurl=https%3A%2F%2Fletsenhance.io%2Fstatic%2F73136da51c245e80edc6ccfe44888a99%2F1015f%2FMainBefore.jpg&imgrefurl=https%3A%2F%2Fletsenhance.io%2F&docid=-t22bY2ix3gHaM&tbnid=D2e1clQQJsbJwM&vet=12ahUKEwjRx__I6JqLAxVyxzgGHYKKCPYQM3oECHIQAA..i&w=1280&h=720&hcb=2&ved=2ahUKEwjRx__I6JqLAxVyxzgGHYKKCPYQM3oECHIQAA'},
-        {id:3,image:'https://www.google.com/imgres?q=images&imgurl=https%3A%2F%2Fmedia.istockphoto.com%2Fid%2F1317323736%2Fphoto%2Fa-view-up-into-the-trees-direction-sky.jpg%3Fs%3D612x612%26w%3D0%26k%3D20%26c%3Di4HYO7xhao7CkGy7Zc_8XSNX_iqG0vAwNsrH1ERmw2Q%3D&imgrefurl=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fnature&docid=tnVTsEa64LdCyM&tbnid=EE4aovnciYd7tM&vet=12ahUKEwjRx__I6JqLAxVyxzgGHYKKCPYQM3oECFgQAA..i&w=612&h=408&hcb=2&ved=2ahUKEwjRx__I6JqLAxVyxzgGHYKKCPYQM3oECFgQAA'}
+image:'https://images.ctfassets.net/hrltx12pl8hq/28ECAQiPJZ78hxatLTa7Ts/2f695d869736ae3b0de3e56ceaca3958/free-nature-images.jpg?fit=fill&w=1200&h=630'        },
+        {id:2,
+            image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtnvAOajH9gS4C30cRF7rD_voaTAKly2Ntaw&s'},
+        {id:3,image:'https://pixlr.com/images/generator/how-to-generate.webp'}
     ]
 }
